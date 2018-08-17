c61312abd035f58cb36087412e1ed9d1a36921dd
maj4e <maja.taseska@esat.kuleuven.be>
2018-08-17 11:32:43 +0200

    modified gitignore

---
+++ login.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Audiocapture
+//
+//  Created by Maja Taseska on 17/08/2018.
+//  Copyright © 2018 Maja Taseska. All rights reserved.
+//
+
+import Foundation
+
+let spotturl  =  "https://cms-api-tst.spott.tv/rest/v004/system/files/uploads"
+let testtoken = "ec0a42a4-2f83-4e1e-913f-a8b3327705d8"
+
